01eb5cf8b153778a9a885f4885be2c6ce4ef7e5e
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:22:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q84
+//this function replce text in a string
+function replacejavawithtypesripit(text) {
+    return text.replace(/java/g, "typeScript");
+}
+console.log(replacejavawithtypesripit("I love java"));
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q84
+//this function replce text in a string
+function replacejavawithtypesripit(text: string): string {
+    return text.replace(/java/g , "typeScript");
+}
+console.log(replacejavawithtypesripit("I love java"));
